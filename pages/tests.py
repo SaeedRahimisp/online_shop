cd987d5ed05e27427ed9f3941adcc6997ab371d7
Saeed <133646450+SaeedRahimisp@users.noreply.github.com>
2023-06-06 12:07:00 +0330

    Add tests to Pages.tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomePageTests(TestCase):
+    def test_home_page_url_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_url(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_template_used(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'home.html')
+
+class AboutUsPageTests(TestCase):
+    def test_aboutus_page_url(self):
+        response = self.client.get('/aboutus/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_aboutus_page_url_name(self):
+        response = self.client.get(reverse('aboutus'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_aboutus_page_template_used(self):
+        response = self.client.get(reverse('aboutus'))
+        self.assertTemplateUsed(response, 'pages/aboutus.html')
+
