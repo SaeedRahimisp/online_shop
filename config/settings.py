4931d70ffe37fee39b8073e980a2474abb4d66fd
Saeed <133646450+SaeedRahimisp@users.noreply.github.com>
2023-06-06 17:21:07 +0330

    Add products and product model

--- config/settings.py
+++ config/settings.py
@@ -40,15 +40,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # installed apps
+    'accounts.apps.AccountsConfig',
+    'pages.apps.PagesConfig',
+    'products.apps.ProductsConfig',
+
     # third party apps
     'crispy_forms',
     'crispy_bootstrap4',
     'allauth',
     'allauth.account',
 
-    # installed apps
-    'accounts',
-    'pages',
 ]
 
 SITE_ID = 1
