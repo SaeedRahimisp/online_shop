a525c8281aa19a6633bde79bc04e46270a7a5ab2
Saeed <133646450+SaeedRahimisp@users.noreply.github.com>
2023-06-06 19:27:00 +0330

    Add Comment Model to Products Model

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 class Product(models.Model):
     title = models.CharField(max_length=50)
@@ -10,8 +11,22 @@ class Product(models.Model):
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.title
-
     def get_absolute_url(self):
         return reverse('product_detail', args=[self.pk])
+
+class Comment(models.Model):
+    PRODUCT_STARS = [
+        ('1', 'Very Bad'),
+        ('2', 'Bad'),
+        ('3', 'Normal'),
+        ('4', 'Good'),
+        ('5', 'Perfect'),
+    ]
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments')
+    body = models.TextField(max_length=200)
+    stars = models.CharField(max_length=10, choices=PRODUCT_STARS)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
