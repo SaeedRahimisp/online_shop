9a210c9be1afb2b176994564f4f7db2eb9db422a
Saeed <133646450+SaeedRahimisp@users.noreply.github.com>
2023-06-07 17:54:07 +0330

    Add comments

--- products/models.py
+++ products/models.py
@@ -30,3 +30,6 @@ class Comment(models.Model):
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.product.id])
