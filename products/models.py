a88f2047cb171a0c34ae0a9fb0feae8429125822
Saeed <133646450+SaeedRahimisp@users.noreply.github.com>
2023-06-07 20:00:15 +0330

    Add translation to product app

--- products/models.py
+++ products/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 class Product(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.TextField()
-    price = models.PositiveIntegerField(default=0)
+    title = models.CharField(max_length=50, verbose_name=_('Title'))
+    description = models.TextField(verbose_name=_('Description'))
+    price = models.PositiveIntegerField(default=0, verbose_name=_('Price'))
     active = models.BooleanField(default=True)
 
-    datetime_created = models.DateTimeField(auto_now_add=True)
-    datetime_modified = models.DateTimeField(auto_now=True)
+    datetime_created = models.DateTimeField(auto_now_add=True, verbose_name=_('Create date'))
+    datetime_modified = models.DateTimeField(auto_now=True, verbose_name=_('Update date'))
 
     def get_absolute_url(self):
         return reverse('product_detail', args=[self.pk])
@@ -20,20 +21,21 @@ class ActiveCommentManager(models.Manager):
 
 class Comment(models.Model):
     PRODUCT_STARS = [
-        ('1', 'Very Bad'),
-        ('2', 'Bad'),
-        ('3', 'Normal'),
-        ('4', 'Good'),
-        ('5', 'Perfect'),
+        ('1', _('Very Bad')),
+        ('2', _('Bad')),
+        ('3', _('Normal')),
+        ('4', _('Good')),
+        ('5', _('Perfect')),
     ]
-    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments')
-    body = models.TextField(verbose_name='Comment Text', max_length=200)
-    stars = models.CharField(max_length=10, choices=PRODUCT_STARS, verbose_name='What is your score?')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(get_user_model(),
+                               on_delete=models.CASCADE, related_name='comments', verbose_name=_('Comment author'))
+    body = models.TextField(verbose_name=_('Comment Text'), max_length=200)
+    stars = models.CharField(max_length=10, choices=PRODUCT_STARS, verbose_name=_('What is your score?'))
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments', verbose_name=_('Product'))
 
-    datetime_created = models.DateTimeField(auto_now_add=True)
-    datetime_modified = models.DateTimeField(auto_now=True)
-    active = models.BooleanField(default=True)
+    datetime_created = models.DateTimeField(auto_now_add=True, verbose_name=_('Create date'))
+    datetime_modified = models.DateTimeField(auto_now=True, verbose_name=_('Update date'))
+    active = models.BooleanField(default=True, verbose_name=_('Active'))
 
     # manager
     objects = models.Manager()
