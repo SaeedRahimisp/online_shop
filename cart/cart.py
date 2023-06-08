4a9588c25e5bfe05b4ba70980613619e86b5700b
Saeed <133646450+SaeedRahimisp@users.noreply.github.com>
2023-06-08 23:54:03 +0330

    Add Cart app and cart settings

---
+++ cart/cart.py
@@ -0,0 +1,43 @@
+
+class Cart:
+    def __int__(self, request):
+        """"
+        Initialize the cart
+        """
+        self.request = request
+        self.session = request.session
+        cart = self.session.get('cart')
+
+        if not cart:
+            cart = self.session['cart'] = {}
+
+        self.cart = cart
+
+    def add(self, product, quantity=1):
+        """"
+        Add the specified product to the cart
+        """
+        product_id = str(product.id)
+
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': quantity}
+        else:
+            self.cart[product_id]['quantity'] += quantity
+
+        self.save()
+
+    def remove(self, product, quantity=1):
+        """"
+        Remove the specified product from the cart
+        """
+        product_id = str(product.id)
+
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def save(self):
+        """"
+        Save the cart's changes
+        """
+        self.session.modified = True
